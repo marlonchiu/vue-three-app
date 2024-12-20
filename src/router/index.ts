815832c8be74d083c7e08ebc08a41270d18bd258
marlonchiu <Knight1898@outlook.com>
2024-12-20 11:26:31 +0800

    调整文件结构

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,18 +3,18 @@ import GetStarted from '@/views/GetStarted.vue'
 
 const routes: Array<RouteRecordRaw> = [
   { path: '/', component: GetStarted },
-  { path: '/s-1', component: () => import('@/views/Shader-1.vue') },
-  { path: '/s-2', component: () => import('@/views/Shader-2.vue') },
-  { path: '/s-3', component: () => import('@/views/Shader-3.vue') },
-  { path: '/s-4', component: () => import('@/views/Shader-4.vue') },
-  { path: '/s-5', component: () => import('@/views/Shader-5.vue') },
-  { path: '/s-6', component: () => import('@/views/Shader-6.vue') },
-  { path: '/s-7', component: () => import('@/views/Shader-7.vue') },
-  { path: '/s-8', component: () => import('@/views/Shader-8.vue') },
-  { path: '/d-1', component: () => import('@/views/Particle.vue') },
-  { path: '/d-2', component: () => import('@/views/Bravo.vue') },
-  { path: '/d-3', component: () => import('@/views/HandDrawn.vue') },
-  { path: '/d-4', component: () => import('@/views/HandDrawn2.vue') },
+  { path: '/a-1', component: () => import('@/views/article/Shader-1.vue') },
+  { path: '/a-2', component: () => import('@/views/article/Shader-2.vue') },
+  { path: '/a-3', component: () => import('@/views/article/Shader-3.vue') },
+  { path: '/a-4', component: () => import('@/views/article/Shader-4.vue') },
+  { path: '/a-5', component: () => import('@/views/article/Shader-5.vue') },
+  { path: '/a-6', component: () => import('@/views/article/Shader-6.vue') },
+  { path: '/a-7', component: () => import('@/views/article/Shader-7.vue') },
+  { path: '/a-8', component: () => import('@/views/article/Shader-8.vue') },
+  { path: '/d-1', component: () => import('@/views/demo/Particle.vue') },
+  { path: '/d-2', component: () => import('@/views/demo/Bravo.vue') },
+  { path: '/d-3', component: () => import('@/views/demo/HandDrawn.vue') },
+  { path: '/d-4', component: () => import('@/views/demo/HandDrawn2.vue') }
 ]
 
 const router = createRouter({
